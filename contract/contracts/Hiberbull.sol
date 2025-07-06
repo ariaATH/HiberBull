bd34db040852c82251b094d85b6ac8110d8b444a
ATH <aria123sparco@gmail.com>
2025-07-06 16:42:09 +0330

    feat: add Hardhat setup and initial Lock contract tests
    - Created package.json with Hardhat and necessary dependencies.
    - Implemented comprehensive tests for the Lock contract, covering deployment, withdrawal validations, events, and fund transfers.

---
+++ contract/contracts/Hiberbull.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+/**
+ * @title Hiberbull Token
+ * @dev ERC20 Token with a fixed supply, owned by the deployer.
+ */
+contract Hiberbull is ERC20, Ownable {
