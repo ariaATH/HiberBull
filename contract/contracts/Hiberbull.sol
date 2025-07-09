8b454cad3c4f556e52a254ebf8f0dff7110e0b63
ATH <aria123sparco@gmail.com>
2025-07-09 12:51:37 +0330

    Refactor Hiberbull contract constructor and update deployment module

--- contract/contracts/Hiberbull.sol
+++ contract/contracts/Hiberbull.sol
@@ -3,21 +3,25 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IUniswapV2Router.sol";
+
 /**
  * @title Hiberbull Token
  * @dev ERC20 Token with a fixed supply, owned by the deployer.
  */
 contract Hiberbull is ERC20, Ownable {
-    address public constant burnAddress = 0x000000000000000000000000000000000000dEaD;
-    uint256 public burnFee = 1;  // 1%
-    int256 public liquidityFee = 4; // 4%  
-    uint256 totalSupply = 10000000000 * 10 ** decimals();
-    
-    constructor(address liquiditywallet , address marketingwallet)ERC20("HiberBull" , "HBLX"){
-        
-        __mint(address(this), totalSupply); // mint all to contract first
-        _burn(burnAddress , (20 * totalSupply) / 100 ) 
-        _transfer(address(this), _liquidityWallet, liquidityAmount); // send 70% to liquidity wallet
-        _transfer(address(this), _marketingWallet, marketingAmount); // send 10% to marketing wallet
-    
+    address public constant burnAddress =
+        0x000000000000000000000000000000000000dEaD;
+    uint256 public burnFee = 1; // 1%
+    uint256 public liquidityFee = 4; // 4%
+
+    constructor(
+        address _liquiditywallet,
+        address _marketingwallet
+    ) ERC20("Hiberbull", "HBLX") Ownable(msg.sender) {
+        uint256 totalSupply = 10000000000 * 10 ** decimals();
+        _mint(address(this), totalSupply); // mint all to contract first
+        _transfer(address(this), burnAddress, (20 * totalSupply) / 100); // send 20% to burn address
+        _transfer(address(this), _liquiditywallet, (totalSupply * 70) / 100); // send 70% to liquidity wallet
+        _transfer(address(this), _marketingwallet, (totalSupply * 10) / 100); // send 10% to marketing wallet
     }
+}
--- contract/ignition/modules/Hiberbull.js
+++ contract/ignition/modules/Hiberbull.js
@@ -10,7 +10,7 @@ module.exports = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  const lock = m.contract("Lock", [unlockTime], {
+  const Hiberbull = m.contract("Hiberbull", [unlockTime], {
     value: lockedAmount,
   });
 
