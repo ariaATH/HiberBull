f8e41c9e10b94ffc23c8a921cd7e67d62ec15136
ATH <aria123sparco@gmail.com>
2025-07-09 11:51:51 +0330

    Refactor Hiberbull contract constructor and minting logic

--- contract/contracts/Hiberbull.sol
+++ contract/contracts/Hiberbull.sol
@@ -10,4 +10,14 @@ import "./interfaces/IUniswapV2Router.sol";
 contract Hiberbull is ERC20, Ownable {
     address public constant burnAddress = 0x000000000000000000000000000000000000dEaD;
     uint256 public burnFee = 1;  // 1%
-    int256 public liquidityFee = 4; // 4%
+    int256 public liquidityFee = 4; // 4%  
+    uint256 totalSupply = 10000000000 * 10 ** decimals();
+    
+    constructor(address liquiditywallet , address marketingwallet)ERC20("HiberBull" , "HBLX"){
+        
+        __mint(address(this), totalSupply); // mint all to contract first
+        _burn(burnAddress , (20 * totalSupply) / 100 ) 
+        _transfer(address(this), _liquidityWallet, liquidityAmount); // send 70% to liquidity wallet
+        _transfer(address(this), _marketingWallet, marketingAmount); // send 10% to marketing wallet
+    
+    }
