160437ef76552b7fe26eb15adbfb10c7dd33cd69
ATH <aria123sparco@gmail.com>
2025-07-15 10:05:03 +0330

    Enhance fee handling by adding marketing fee logic and tax-free address management functions

--- contract/contracts/Hiberbull.sol
+++ contract/contracts/Hiberbull.sol
@@ -31,6 +31,8 @@ contract Hiberbull is ERC20, Ownable {
         _transfer(address(this), _marketingwallet, (totalSupply * 10) / 100); // send 10% to marketing wallet
     }
 
+    // Override the _update function to implement tax logic , if your address is not tax free
+    // you will pay 1% burn, 2% liquidity and 1% marketing fee
     function _update(
         address from,
         address to,
@@ -41,10 +43,48 @@ contract Hiberbull is ERC20, Ownable {
         } else {
             uint256 amount_liquidity = (liquidityFee * value) / 100; // 2% for holders and 2% for marketing
             uint256 amount_burn = (burnFee * value) / 100; // 1% for burn
+            uint256 amount_marketing = (marketingfee * value) / 100; // 1% for marketing
             value -= (amount_burn + amount_liquidity);
             super._update(from, _liquiditywallet, amount_liquidity);
             super._burn(from, amount_burn);
+            super._update(from, _marketingwallet, amount_marketing);
             super._update(from, to, value);
         }
+    }
+
+    function isTaxFree(address _address) external view returns (bool) {
+        return tax_free[_address];
+    }
+
+    function setTaxFree(address _address, bool _status) external onlyOwner {
+        tax_free[_address] = _status;
+    }
+
+    function setBurnFee(uint256 _burnFee) external onlyOwner {
+        burnFee = _burnFee;
+    }
+
+    function setLiquidityFee(uint256 _liquidityFee) external onlyOwner {
+        liquidityFee = _liquidityFee;
+    }
+
+    function setMarketingFee(uint256 _marketingFee) external onlyOwner {
+        marketingfee = _marketingFee;
+    }
+
+    function setLiquidityWallet(address liquiditywallet) external onlyOwner {
+        _liquiditywallet = liquiditywallet;
+    }
+
+    function setMarketingWallet(address marketingwallet) external onlyOwner {
+        _marketingwallet = marketingwallet;
+    }
+
+    function getLiquidityWallet() external view returns (address) {
+        return _liquiditywallet;
+    }
+
+    function getMarketingWallet() external view returns (address) {
+        return _marketingwallet;
     }
 }
