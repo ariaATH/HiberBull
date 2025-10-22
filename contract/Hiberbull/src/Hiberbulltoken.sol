fee1f4fdb065d0291d4b8bcb82ccdf7e56397f96
ATH <aria123sparco@gmail.com>
2025-10-22 19:05:42 +0330

    Refactor Hiberbulltoken contract to implement tax fee mechanism and remove Counter script and tests

--- contract/Hiberbull/src/Hiberbulltoken.sol
+++ contract/Hiberbull/src/Hiberbulltoken.sol
@@ -4,7 +4,35 @@ import "lib/openzeppelin-contracts-5.4.0/contracts/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts-5.4.0/contracts/access/Ownable.sol";
 
 contract Hiberbulltoken is ERC20, Ownable {
-    constructor() ERC20("Hiberbull", "HIBER") {
+    // Tax fee and wallet address
+    uint16 private taxfee;
+    // Tax wallet address
+    address private taxWallet;
+    // Wallet tax-free status
+    mapping(address => bool) private wallettaxfree;
+
+    constructor(
+        address _taxWallet,
+        uint16 _taxfee
+    ) ERC20("Hiberbull", "HIBER") Ownable(msg.sender) {
+        taxWallet = _taxWallet;
+        taxfee = _taxfee;
+        wallettaxfree[msg.sender] = true;
+        wallettaxfree[taxWallet] = true;
         _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    // Override transfer function to include tax
+    function transfer(
+        address to,
+        uint256 amount
+    ) public override returns (bool) {
+        if (!wallettaxfree[msg.sender]) {
+            _transfer(msg.sender, to, amount - ((amount * taxfee) / 100));
+            _transfer(msg.sender, taxWallet, (amount * taxfee) / 100);
+        } else {
+            _transfer(msg.sender, to, amount);
+        }
+        return true;
     }
 }
