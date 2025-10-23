f2bde4bceba22bc202912318a8891b26f30c1432
ATH <aria123sparco@gmail.com>
2025-10-23 09:55:34 +0330

    Remove outdated test files for Heap, MerkleTree, and Time; update import paths in Hiberbulltoken contract to use the latest OpenZeppelin contracts.

--- contract/Hiberbull/src/Hiberbulltoken.sol
+++ contract/Hiberbull/src/Hiberbulltoken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "lib/openzeppelin-contracts-5.4.0/contracts/token/ERC20/ERC20.sol";
-import "lib/openzeppelin-contracts-5.4.0/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Hiberbulltoken is ERC20, Ownable {
     // Tax fee and wallet address
