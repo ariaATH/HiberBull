5a6db0ab6593a407b11eee8169f2f6cf2948e33c
ATH <aria123sparco@gmail.com>
2025-10-25 15:57:06 +0330

    Refactor Hiberbulltoken and Staketoken contracts to initialize tax wallet correctly and add Transfercompleted event for transfer tracking

--- contract/Hiberbull/src/Hiberbulltoken.sol
+++ contract/Hiberbull/src/Hiberbulltoken.sol
@@ -2,24 +2,31 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Staketoken.sol";
 
 contract Hiberbulltoken is ERC20, Ownable {
+    Staketoken private staketoken;
     // Tax fee and wallet address
     uint16 private taxfee;
     // Tax wallet address
-    address private taxWallet;
+    address private taxWallet = staketoken.getstakingWallet();
     // Wallet tax-free status
     mapping(address => bool) private wallettaxfree;
 
     constructor(
         uint16 _taxfee
     ) ERC20("Hiberbull", "HIBER") Ownable(msg.sender) {
-        taxWallet = address(this);
         taxfee = _taxfee;
         wallettaxfree[msg.sender] = true;
         wallettaxfree[taxWallet] = true;
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
+
+    event Transfercompleted(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
 
     // Override transfer function to include tax
     function transfer(
@@ -28,9 +35,15 @@ contract Hiberbulltoken is ERC20, Ownable {
     ) public override returns (bool) {
         if (!wallettaxfree[msg.sender]) {
             _transfer(msg.sender, to, amount - ((amount * taxfee) / 100));
+            emit Transfercompleted(
+                msg.sender,
+                to,
+                amount - ((amount * taxfee) / 100)
+            );
             _transfer(msg.sender, taxWallet, (amount * taxfee) / 100);
         } else {
             _transfer(msg.sender, to, amount);
+            emit Transfercompleted(msg.sender, to, amount);
         }
         return true;
     }
--- contract/Hiberbull/src/Staketoken.sol
+++ contract/Hiberbull/src/Staketoken.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Staketoken {}
+contract Staketoken is Ownable {
+    address private immutable stakingWallet;
+
+    constructor() Ownable(msg.sender) {
+        stakingWallet = address(this);
+    }
+
+    function getstakingWallet() external view returns (address) {
+        return stakingWallet;
+    }
+}
