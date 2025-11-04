1223ba7c9797565ac98934cb30fedb3ea30be9a3
ATH <aria123sparco@gmail.com>
2025-11-04 12:09:28 +0330

    Implement staking functionality in Stakeholders contract with token balance check

--- contract/Hiberbull/src/Stakeholders.sol
+++ contract/Hiberbull/src/Stakeholders.sol
@@ -1,15 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// The interface for the staking rewards contract
+// stakeholders get 1/3 of the tax of transactions that monthly get from the users
+// The contract allows users to stake their tokens and earn rewards they stake token for 1 month and
+// they can claim their rewards after the staking period ends
 
 contract Stakeholders is Ownable {
+    // The wallet where staked tokens are held
     address private immutable stakingWallet;
+    // The ERC20 token being staked
+    IERC20 private immutable token;
+    // Total amount of tokens staked
+    uint256 private totalStaked;
+    // Mapping of user addresses to their staked token balances
+    mapping(address => uint256) private stakedBalances;
 
-    constructor() Ownable(msg.sender) {
+    error NotEnoughTokens();
+
+    constructor(address tokenAddress) Ownable(msg.sender) {
         stakingWallet = address(this);
+        token = IERC20(tokenAddress);
     }
 
-    function getstakingWallet() external view returns (address) {
-        return stakingWallet;
+    function staketokenonemonth(uint256 amount) external {
+        if(token.balanceOf(msg.sender) < amount) {
+            revert NotEnoughTokens();
+        }
     }
+
+
 }
